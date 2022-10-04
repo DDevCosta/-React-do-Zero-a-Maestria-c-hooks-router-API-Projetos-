505fb680935df49e5dbdea8dc6ede46d4b55fc47
DDevCosta <DDevCosta@gmail.com>
2022-10-04 11:01:01 -0300

    seção 5

--- Seção 5/form/src/components/MyForm.js
+++ Seção 5/form/src/components/MyForm.js
@@ -1,16 +1,38 @@
 import './MyForm.css'
+import {useState} from 'react'
 
 const MyForm = () => {
+
+  const [name,setName] =useState()
+  const [email,setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name)
+  console.log(email)
+
   return (
     <div>
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="nome" placeholder="Digite seu nome"/>
+                <input 
+                type="text" 
+                name="name" 
+                placeholder="Digite seu nome"
+                onChange={handleName}
+                />
             </div>
             <label>
                 <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite seu email"/>
+                <input 
+                  type="email" 
+                  name="email" 
+                  placeholder="Digite seu email" 
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
             </label>
             <input type="submit" value="Enviar"/>
         </form>
